1553a8ca313106838036fa4e959b127215ca142c
Mateusz Dejewski <matdejewski@gmail.com>
2023-09-30 23:09:40 +0200

    setup relations via fluent api

--- open-ai-chat-infrastructure/ApplicationDbContext.cs
+++ open-ai-chat-infrastructure/ApplicationDbContext.cs
@@ -12,4 +12,19 @@ public class ApplicationDbContext : DbContext
     public DbSet<User> Users { get; set; }
     public DbSet<ChatConfiguration> ChatConfigurations { get; set; }
     public DbSet<Conversation> Conversations { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<User>()
+            .HasMany(p => p.ChatConfigurations)
+            .WithOne(p => p.User)
+            .HasForeignKey(p => p.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Entity<User>()
+            .HasMany(p => p.Conversations)
+            .WithOne(p => p.User)
+            .HasForeignKey(p => p.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
 }
